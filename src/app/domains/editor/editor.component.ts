34a67b12637fcde2c474bbf03180d0d217a85bfa
AMSaiian <artursaiyan56@gmail.com>
2025-04-30 13:01:37 +0300

    editor PoC

--- src/app/domains/editor/editor.component.ts
+++ src/app/domains/editor/editor.component.ts
@@ -1,16 +1,17 @@
 import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable, switchMap, tap } from 'rxjs';
 import { EdgeDto, InsertionDto, NodeDto } from '../../common/dto/layout.dto';
 import {
+  AssignElement,
   ConditionElement,
   ForLoopElement,
   ProcedureElement,
   TerminalElement,
 } from '../../common/models/element/element.model';
+import { BoolExpression, BoolExpressionType } from '../../common/models/expression/expression.model';
 import { InstanceofPipe } from '../../common/pipes/instance-of.pipe';
 import { AppStateService } from '../../common/services/app-state.service';
-import { Observable, switchMap, tap } from 'rxjs';
-// import { EdgeDTO } from '../../common/utils/layout.utils';
 
 @Component({
   selector: 'app-editor',
@@ -46,4 +47,16 @@ export class EditorComponent implements OnInit {
   protected readonly ConditionElement = ConditionElement;
   protected readonly ProcedureElement = ProcedureElement;
   protected readonly ForLoopElement = ForLoopElement;
+
+  public handleInsert(point: InsertionDto) {
+    // const element = new AssignElement(new AssignExpression('x', '1', false));
+    const element = new ConditionElement(new BoolExpression('x', BoolExpressionType.Equals, 'x'))
+    this.state.addElement(element, point.scopeId, point.fromId);
+  }
+
+  protected readonly AssignElement = AssignElement;
+
+  public deleteNode(id: string) {
+    this.state.deleteElement(id);
+  }
 }
